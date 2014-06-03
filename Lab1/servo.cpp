dd6d5775ff5b22b668165061e8d9a1fb07f56f94
DrHaXX <c0nd3mn3d@hotmail.com>
2014-06-03 11:23:39 +0200

    MURT

--- Lab1/servo.cpp
+++ Lab1/servo.cpp
@@ -17,11 +17,12 @@ void turn(DigitalOut *motor, float duration) {
 
 int main() {
 	printf("Press 1 to turn the servo left, and 2 for right.\r\n");
+	printf("Initial test: ml = %d, mr = %d.\r\n", ml.read(), mr.read());
 
 	while(1) {
 		switch(pc.getc()) {
-			case '1': turn(&ml, 0.1); printf("Turned left\r\n"); break;
-			case '2': turn(&mr, 0.1); printf("Turned right\r\n"); break;
+			case '1': turn(&ml, 0.05); printf("Turned left\r\n"); break;
+			case '2': turn(&mr, 0.05); printf("Turned right\r\n"); break;
 			default: break;
 		}
 	}
