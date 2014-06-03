618a57a5ba7659adc9eaf49ceb9b42c4502cf4ee
DrHaXX <c0nd3mn3d@hotmail.com>
2014-06-03 10:49:16 +0200

    U MAD FGT 2.0

---
+++ Lab1/servo.cpp
@@ -0,0 +1,28 @@
+#include "mbed.h"
+#include "Servo.h"
+
+#define ON 1
+#define OFF 0
+
+Serial pc(USBTX, USBRX);
+
+DigitalOut ml(p18);
+DigitalOut mr(p15);
+
+void turn(DigitalOut *motor, float duration) {
+	*motor = ON;
+	wait(duration);
+	*motor = OFF;
+}
+
+int main() {
+	printf("Press 1 to turn the servo left, and 2 for right.\r\n");
+
+	while(1) {
+		switch(pc.getc()) {
+			case '1': turn(&ml, 0.1); printf("Turned left\r\n"); break;
+			case '2': turn(&mr, 0.1); printf("Turned right\r\n"); break;
+			default: break;
+		}
+	}
+}
