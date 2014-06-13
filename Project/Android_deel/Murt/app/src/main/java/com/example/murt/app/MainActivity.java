e30ad4de7f0d9f1e132d349f65048e5b79da6099
DrHaXX <c0nd3mn3d@hotmail.com>
2014-06-13 15:38:28 +0200

    kleine toast toegevoegd voor bij je ontbijt

--- Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/MainActivity.java
+++ Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -141,6 +142,10 @@ public class MainActivity extends Activity {
 
 			    /* We do not send the splitted imgs, as they are saved in temporary files or recreated in the new activity. */
 			    startActivity(intent);
+
+			    /* Show a toast for better user experience (not that we care about that) :P */
+			    Toast toast = Toast.makeText(getApplicationContext(), "Click to rotate", Toast.LENGTH_SHORT);
+			    toast.show();
 		    }
 	    });
     }
