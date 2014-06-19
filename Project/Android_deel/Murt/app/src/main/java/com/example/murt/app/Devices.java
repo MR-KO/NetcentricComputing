a5b44a3ffda8dd4a042d32edbfe5f38bca8a5635
Timo <timo.dobber@student.uva.nl>
2014-06-19 12:48:05 +0200

    Now in list mode

--- Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/Devices.java
+++ Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/Devices.java
@@ -1,9 +1,11 @@
 package com.example.murt.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Devices {
 
-	public static String[] deviceStrings = {
-			"MurtDevice1", "MurtDevice2", "MurtDevice3", "MurtDevice4", "MurtDevice5", "MurtDevice6"
-	};
+    public static final List<String> deviceStrings = Arrays.asList("Device1", "Device2", "Device3",
+            "Device4", "Device5", "Device6");
 
 }
