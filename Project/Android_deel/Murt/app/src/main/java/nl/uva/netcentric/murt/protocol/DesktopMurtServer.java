973692713cb0ea8fa9bd7f696369a61d88a073df
sbouber <sjoerdbouber@hotmail.com>
2014-06-19 13:09:49 +0200

    ...

---
+++ Project/Android_deel/Murt/app/src/main/java/nl/uva/netcentric/murt/protocol/DesktopMurtServer.java
@@ -0,0 +1,44 @@
+package nl.uva.netcentric.murt.protocol;
+
+/**
+ * Created by Sjoerd on 18-6-2014.
+ */
+public class DesktopMurtServer extends AbstractMurtServer {
+
+
+    public DesktopMurtServer(int port) {
+        super(port, new MurtConnectionListener() {
+
+            @Override
+            public void onReceive(byte[] data) {
+
+            }
+
+            @Override
+            public void onConnect(MurtConnection conn) {
+                System.out.println("MurtConnectionListener onConnect");
+            }
+
+            @Override
+            public void onDisconnect(MurtConnection conn) {
+                System.out.println("MurtConnectionListener onDisconnect");
+            }
+
+            @Override
+            public byte[] onSend(MurtConnection conn) {
+                System.out.println("MurtConnectionListener onSend");
+                return new byte[] {1, 3, 3, 7};
+            }
+        });
+    }
+
+    @Override
+    protected void log(String message) {
+        System.out.println(message);
+    }
+
+
+    public static void main(String[] args) {
+        new DesktopMurtServer(MurtConfiguration.DEBUG_PORT);
+    }
+}
