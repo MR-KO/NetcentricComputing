dd43f9b542a37ec9f722e33acf935930bf8347a2
DrHaXX <c0nd3mn3d@hotmail.com>
2014-06-26 23:03:32 +0200

    Removed useless connections hashmap

--- Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/Devices.java
+++ Project/Android_deel/Murt/app/src/main/java/com/example/murt/app/Devices.java
@@ -1,13 +1,10 @@
 package com.example.murt.app;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
 
 public class Devices {
 
 	public static final List<String> deviceStrings = new ArrayList<String>();
-	public static final Map<Integer, String> connections = new Hashtable<Integer, String>();
 	public static boolean initialized = false;
 }
